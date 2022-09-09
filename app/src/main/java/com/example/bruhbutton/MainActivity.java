8353d80887b438d3154f8633ac84c7bc2659d466
braydenwerner <braydenwerner0@gmail.com>
2022-09-08 21:32:53 -0700

    Added bruh sound effect

--- app/src/main/java/com/example/bruhbutton/MainActivity.java
+++ app/src/main/java/com/example/bruhbutton/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.bruhbutton;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.bruhbutton.databinding.ActivityMainBinding;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    private TextView mTextView;
+    //  private TextView mTextView;
     private ActivityMainBinding binding;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,13 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+        mediaPlayer = MediaPlayer.create(this, R.raw.bruh);
+
+        Button bruhButton = findViewById(R.id.bruhButton);
+        bruhButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        mediaPlayer.start();
     }
 }
